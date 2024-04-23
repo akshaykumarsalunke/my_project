614f148f71c8474927a6fea087ffa8057c60f37d
Akshaykumar Salunke <akshaykumar@Akshaykumars-MacBook-Pro.local>
2024-04-23 15:42:56 +0530

    changes to the dag

--- dags/stress_test.py
+++ dags/stress_test.py
@@ -1,10 +1,20 @@
-from airflow.models import DAG
+from airflow.decorators import dag
 from airflow.operators.bash import BashOperator
 from datetime import datetime
 
-with DAG('stress-dag', schedule_interval=None,
-        start_date=datetime(2022, 1, 1), catchup=False) as dag:
- 
+@dag(
+    start_date=datetime(2024, 1, 1),
+    schedule="@daily",
+    catchup=False,
+    doc_md=__doc__,
+    default_args={"owner": "Astro", "retries": 3},
+    tags=["example"],
+)
+def stress_dag():
+
    stress = BashOperator(task_id='stress-dag',
                          bash_command='stress-ng --cpu 1 -v --timeout 10m'
                          )
+   
+# Instantiate the DAG
+stress_dag()
