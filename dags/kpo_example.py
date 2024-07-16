514ef80444fd6e728d9ef30d33a63fa175e0a2b7
akshaykumarsalunke <165752380+akshaykumarsalunke@users.noreply.github.com>
2024-07-16 15:51:32 +0530

    Update kpo_example.py

--- dags/kpo_example.py
+++ dags/kpo_example.py
@@ -20,16 +20,14 @@ def kpo_example():
         task_id="kpo_task",
         name="kpo-test-pod",
         image=YOUR_DOCKER_IMAGE,
-        in_cluster=False,  # if you are not running Airflow on K8s already you will need to adjust the KPO parameters to connect to your cluster
+        in_cluster=False,  
         cluster_context="/usr/local/airlfow/include/.kube/config",
+        kubernetes_conn_id="kube_external",
         get_logs=True,
         log_events_on_failure=True,
         is_delete_operator_pod=True,
         # make sure XComs are pushed
         do_xcom_push=True,
     )
-
     kpo_task 
-
-
 kpo_example()
