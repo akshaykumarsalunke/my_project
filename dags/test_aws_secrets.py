c8e41f5092b4dd6b191b1309e8d0485775f0107f
akshaykumarsalunke <165752380+akshaykumarsalunke@users.noreply.github.com>
2024-08-05 23:55:39 +0530

    Create test_aws_secrets.py

---
+++ dags/test_aws_secrets.py
@@ -0,0 +1,22 @@
+from datetime import datetime
+
+from airflow import DAG
+from airflow.hooks.base import BaseHook
+from airflow.models import Variable
+from airflow.operators.python import PythonOperator
+
+
+def print_var():
+    my_var = Variable.get("my_var")
+    print(f'My variable is: {my_var}')
+
+    conn = BaseHook.get_connection(conn_id="my_db")
+    print(conn.get_uri())
+
+
+with DAG('example_secrets_dag', start_date=datetime(2022, 1, 1), schedule=None) as dag:
+
+  test_task = PythonOperator(
+      task_id='test-task',
+      python_callable=print_var
+)
