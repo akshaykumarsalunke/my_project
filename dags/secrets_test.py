8d8cdbbe3a8c18ec4ff1fa63cf984a0e66fa0cca
akshaykumarsalunke <165752380+akshaykumarsalunke@users.noreply.github.com>
2024-08-07 16:59:00 +0530

    Create secrets_test.py

---
+++ dags/secrets_test.py
@@ -0,0 +1,17 @@
+from airflow.decorators import dag, task
+from datetime import datetime
+from airflow.providers.amazon.aws.secrets.secret_mananger import SecretsManagerBackend
+
+@dag(
+  "secret_test",
+  schedule=None,
+  tags=['secret'],
+  catchup=False
+)
+def secret_dag():
+  @task()
+  def secret_task:
+    secret_obj = SecretsManagerBackend()
+    sec_client = secret_obj.client()
+
+dag_obj = secret_dag()
